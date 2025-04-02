53484764b1714053f57e5b3efd0d6ce40930ff1a
carsten-gasch <carsten.gasch@gmail.com>
2025-04-02 20:11:14 +0200

    refactor to use multiple files

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+from stats import get_num_words
+
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
 
 
-def count_words(string):
-    return len(string.split())
-
-
 def main():
     file_content = get_book_text("books/frankenstein.txt")
-    num_words = count_words(file_content)
+    num_words = get_num_words(file_content)
     print(f"{num_words} words found in the document")
 
 
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_num_words(string):
+    return len(string.split())
